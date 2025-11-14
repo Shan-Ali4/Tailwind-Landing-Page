12a2fbb12a9f775074b81fb547dd1bce2017cbdf
Mirza_Md_Shan_Ali <88309339+Shan-Ali4@users.noreply.github.com>
2025-11-15 02:16:25 +0530

    Updated

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,8 +2,25 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      
+    <div className='flex flex-col gap-12 md:flex-row md:justify-between'>
+      <ul className='flex gap-6 font-lato text-gray-400'>
+        <li>
+          <a href="#"> Facebook</a>
+        </li>
+        <li>
+          <a href="#"> Instragram</a>
+        </li>
+        <li>
+          <a href="#"> Twitter</a>
+        </li>
+      </ul>
+      <div>
+        <img src="./assets/Help-Avatar.svg" alt="help" />
+        <div className='flex gap-2'>
+          <p className='font-playfair font-thin'>Have questions? We're here to help.</p>
+          <a href="#" className='font-lato font-medium'>Talk to a Specialist</a>
+        </div>
+      </div>
     </div>
   )
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,8 +8,16 @@ const Header = () => {
           <img src="./assets/Logo.svg" alt="Logo" />
           <button className='bg-linear-to-r from-orange-400 to-red-400 text-xs text-white px-2.5 py-1.5 rounded-2xl'>Hoster is Hiring</button>
       </div>
-
-      <div>
+      <ul className='hidden lg:flex justify-between items-center font-lato text-gray-400 gap-6 '>
+        <li><a href="#">Plans</a></li>
+        <li><a href="#">Find Domain</a></li>
+        <li><a href="#">Why Hoster</a></li>
+      </ul>
+      <div className='hidden lg:flex justify-center items-center gap-6 font-lato'>
+        <a href="#" className='text-gray-400'>Sign In</a>
+        <button className='rounded-md px-4 py-3 bg-blue-400 hover:bg-blue-600 text-white'>Join Waitlist</button>
+      </div>
+      <div className='lg:hidden'>
           <FaBars />
       </div>
     </div>
