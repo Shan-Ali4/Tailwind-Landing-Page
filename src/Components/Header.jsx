0ab487eac9c93e64862d99c6ff115a6dec08101d
Mirza_Md_Shan_Ali <88309339+Shan-Ali4@users.noreply.github.com>
2025-11-15 01:38:14 +0530

    update in files and code

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { FaBars } from "react-icons/fa6";
 
 const Header = () => {
   return (
-    <div>
-      
+    <div className='flex justify-between'>
+      <div className='flex items-center justify-center gap-2'>
+          <img src="./assets/Logo.svg" alt="Logo" />
+          <button className='bg-linear-to-r from-orange-400 to-red-400 text-xs text-white px-2.5 py-1.5 rounded-2xl'>Hoster is Hiring</button>
+      </div>
+
+      <div>
+          <FaBars />
+      </div>
     </div>
   )
 }
