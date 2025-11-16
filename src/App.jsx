38bf423e34720d53d8ebc22f1760c8be5bd468cd
Mirza_Md_Shan_Ali <88309339+Shan-Ali4@users.noreply.github.com>
2025-11-17 01:29:13 +0530

    for diploy update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import Header from "./Components/Header"
 import Body from "./Components/Body"
 import Footer from "./Components/Footer"
+import { HashRouter, Routes, Route } from "react-router-dom";
+
 
 function App() {
 
   return (
     <>
       <div className="bg-amber-50 min-h-screen px-6 py-3 flex flex-col gap-16 md:px-10 md:py-7 md:justify-between lg:px-20 lg:py-10" >
-        <Header/>
-        <Body/>
-        <Footer/>
+        <HashRouter>
+          <Routes>
+            <Route path="/" element={
+              <>
+                <Header/>
+                <Body/>
+                <Footer/>
+              </>
+            } />
+          </Routes>
+        </HashRouter>
       </div>
     </>
   )
